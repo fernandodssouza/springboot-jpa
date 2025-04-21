877e509d58ef173db04499fcf723d8e91baeee20
Fernando Souza <fernandodavidss@outlook.com>
2025-04-21 18:14:53 -0300

    Order Entity, ISO 8601

---
+++ src/main/java/com/myProjects/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.myProjects.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.myProjects.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+	
+}
