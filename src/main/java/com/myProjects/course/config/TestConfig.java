09a32dcce66505ea2b755212025591a577bbb311
Fernando Souza <fernandodavidss@outlook.com>
2025-04-23 18:46:17 -0300

    payment one-to-one association

--- src/main/java/com/myProjects/course/config/TestConfig.java
+++ src/main/java/com/myProjects/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.myProjects.course.entities.Category;
 import com.myProjects.course.entities.Order;
 import com.myProjects.course.entities.OrderItem;
+import com.myProjects.course.entities.Payment;
 import com.myProjects.course.entities.Product;
 import com.myProjects.course.entities.User;
 import com.myProjects.course.entities.enums.OrderStatus;
@@ -82,6 +83,12 @@ public class TestConfig implements CommandLineRunner {
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
